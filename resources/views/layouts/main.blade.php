f04fa89de1a778382b3d7c7ed446073f1cb6fcb9
Alemhar Salihuddin <alemhar@gmail.com>
2019-10-01 15:05:44 +0800

    updated AppServiceProvider.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -327,7 +327,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- AdminLTE App -->
 <!--script src="dist/js/adminlte.min.js"></script -->
 
-<script type="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
 <!-- script type="/js/adminlte.js"></script -->
 
 </body>
