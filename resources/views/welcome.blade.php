f4fd1bf1eaaff0b01bc3bec4f6386765fb76bc5d
Alemhar Salihuddin <alemhar@gmail.com>
2019-10-01 10:45:08 +0800

    Initial push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Virtual Work Hub ©
+                    Stocks Inventory ©
                 </div>
 
                 <div class="links">
