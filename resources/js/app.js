ea1af29f51ddf6d01c698fa49b4948bba816ac66
Alemhar Salihuddin <alemhar@gmail.com>
2019-10-01 20:20:01 +0800

    updated AppServiceProvider.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -19,6 +19,7 @@ let routes = [
 
 const router = new VueRouter({
   routes // short for `routes: routes`
+  
 })
 
 /**
@@ -33,14 +34,13 @@ const router = new VueRouter({
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-
 const app = new Vue({
     el: '#app',
     router
 });
+
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,7 +20,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
