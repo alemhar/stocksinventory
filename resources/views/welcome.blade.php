04e8eb760856aeb82cbc4e11b2123d71aeebd759
thinkerhut <thinkerhut@gmail.com>
2019-09-30 21:51:41 -0500

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Stocks Inventory</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Stocks Inventory ©
+                    Virtual Work Hub ©
                 </div>
 
                 <div class="links">
