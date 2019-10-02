674176763be5d228405d63ee9f6be28e91426e9b
Alemhar Salihuddin <alemhar@gmail.com>
2019-10-02 20:32:22 +0800

    added api route user resource

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -147,7 +147,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link" style="text-align: center;">
       <img src="img/si_logo2_200x200.png" alt="Stocks Inventory Logo" class="brand-image elevation-3 img-circle"
-           style="opacity: .8"> 
+           style="background-color: white;padding: 2px;/*opacity: .8*/"> 
       <!-- i class="fas fa-cubes" style="font-size: 1.5rem;"></i -->     
       <!-- img-circle  -->     
       <span class="brand-text font-weight-light"  style="font-size: 1.2rem;">Stock Inventory <span style="font-size: 0.6rem;">2.01</span></span>
