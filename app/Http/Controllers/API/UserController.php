bca7c6610070c39892a986b8470240dd9d8e19d5
Alemhar Salihuddin <alemhar@gmail.com>
2019-10-02 19:48:45 +0800

    added api route user resource

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        return ['message' => 'I have receive your data!'];
+        return $request->all();
     }
 
     /**
