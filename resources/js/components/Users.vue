2d7e81f44c5efe7461d55e6dd14d9d6017cbab4a
Alemhar Salihuddin <alemhar@gmail.com>
2019-10-14 21:29:43 +0800

    Added delete feature | Users page

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -175,11 +175,9 @@
 
         created() {
             this.loadUsers();
-            /*
             Fire.on('AfterUserCreate',() => {
               this.loadUsers();
             });
-            */
             //setInterval(() => this.loadUsers(),3000);
         }
     }
