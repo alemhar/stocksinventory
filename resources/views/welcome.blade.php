af3f3444e389d4f1a184bd2d32f7177d2e4923c2
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-19 08:27:06 +0800

    Changed to AIA Books

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Stocks Inventory</title>
+        <title>AIA Books</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Stocks Inventory ©
+                    AIA Books ©
                 </div>
 
                 <div class="links">
