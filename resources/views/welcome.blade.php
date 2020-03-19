d7637e2c1c3181918e100d85aef4d164cece4311
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-19 08:50:00 +0800

    Changed to AIA Books

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    AIA Books ©
+                    AIA Books ®
                 </div>
 
                 <!-- div class="links">
