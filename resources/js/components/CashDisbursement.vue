b0e5a3ae84443851ad684023c118fe9523186f56
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-22 19:49:03 +0800

    Select Option for Payees

--- resources/js/components/CashDisbursement.vue
+++ resources/js/components/CashDisbursement.vue
@@ -359,11 +359,7 @@
                //this.loadUsers();
             });
 
-            VueListen.$on('input',() => {
-              
-               
-               console.log('input');
-            });
+            
             //console.log(this.payees);
 
             //setInterval(() => this.loadUsers(),3000);
