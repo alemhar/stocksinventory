83ef04d1dee5f65a54a0781830a976340c25ac64
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-22 12:00:30 +0800

    Create Payee Controller

--- app/Payee.php
+++ app/Payee.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payee extends Model
 {
-	protected $companyId = '1';
-    //$companyId = (isset(Auth::user()->name) ? Auth::user()->company_id : '');
+	protected $companyId = 1;
+    //isset(Auth::user()->name) ? Auth::user()->company_id : '1';
 	
-	protected $table = 'payees_'.'1';
+	protected $table = 'payees_'.$companyId;
 
 
     protected $fillable = [
