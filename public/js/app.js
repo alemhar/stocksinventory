983e887aa62af7fe984201578cc58bba62a06c05
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-24 23:58:17 +0800

    CD FrontEnd/Form Validation

--- public/js/app.js
+++ public/js/app.js
@@ -75289,11 +75289,11 @@ var es6_number_constructor = __webpack_require__("c5f6");
     selectedOption: function selectedOption() {
       var _this4 = this;
 
-      /*
-      return this.options.find(function (option) {
+      
+      return this.options.find(function (options) {
         return option.value === _this4.optionValue(_this4.value);
       });
-      */
+      
     }
   },
   methods: {
