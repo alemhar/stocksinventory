c05997a8a6689bd32ab032b297ff20c3e06aff1f
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-26 21:34:56 +0800

    CD FrontEnd/Form Validation

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <!--
 This is a starter template page. Use this page to start your new project from
@@ -190,6 +189,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
+
 <script src="{{ asset('js/jquery-3.3.1.js') }}" type="text/javascript"></script>
 <script src="{{ asset('js/datatables/jquery.dataTables.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset( 'js/datatables/dataTables.bootstrap4.min.js') }}" type="text/javascript"></script>
