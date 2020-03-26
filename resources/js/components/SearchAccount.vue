6bd2323fb027e8e3dcec23bb4b0a6344997f1ed6
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-26 21:22:00 +0800

    CD FrontEnd/Form Validation

--- resources/js/components/SearchAccount.vue
+++ resources/js/components/SearchAccount.vue
@@ -128,7 +128,7 @@
 </script>
 <script src="{{ asset('js/jquery-3.3.1.js') }}" type="text/javascript"></script>
 <script src="{{ asset('js/datatables/jquery.dataTables.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset( 'js/datatables/dataTables.bootstrap4.min.js') }}" type="text/javascript"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" type="text/javascript"></script>
 <style>
   .sa-modal-backdrop {
     position: fixed;
