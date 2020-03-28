9151f16c165007fa99dc2d6c55706f0322dcb1ee
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-28 17:23:51 +0800

    Added drop down branch selector on entry form

--- public/js/app.js
+++ public/js/app.js
@@ -2824,8 +2824,16 @@ __webpack_require__.r(__webpack_exports__);
     console.log(document.querySelector('meta[name="user-id"]').getAttribute('content')); //console.log(this.payees);
     //setInterval(() => this.loadUsers(),3000);
 
-    $('.modal').on('hidden.bs.modal', function (e) {
-      $('body').addClass('modal-open');
+    $('.modal').on("hidden.bs.modal", function (e) {
+      if ($('.modal:visible').length) {
+        $('.modal-backdrop').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) - 10);
+        $('body').addClass('modal-open');
+      }
+    }).on("show.bs.modal", function (e) {
+      if ($('.modal:visible').length) {
+        $('.modal-backdrop.in').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) + 10);
+        $(this).css('z-index', parseInt($('.modal-backdrop.in').first().css('z-index')) + 10);
+      }
     });
   },
   components: {//DynamicSelect
--- resources/js/components/CashDisbursement.vue
+++ resources/js/components/CashDisbursement.vue
@@ -751,9 +751,19 @@
 
             //setInterval(() => this.loadUsers(),3000);
 
-            $('.modal').on('hidden.bs.modal', function (e) {
-                $('body').addClass('modal-open');
-             });
+            $('.modal').on("hidden.bs.modal", function (e) {
+                if($('.modal:visible').length)
+                {
+                    $('.modal-backdrop').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) - 10);
+                    $('body').addClass('modal-open');
+                }
+            }).on("show.bs.modal", function (e) {
+                if($('.modal:visible').length)
+                {
+                    $('.modal-backdrop.in').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) + 10);
+                    $(this).css('z-index', parseInt($('.modal-backdrop.in').first().css('z-index')) + 10);
+                }
+            });
         },
         components: {
           //DynamicSelect
