23a51ac0024d9f3884d3f79b0d198e6b1b87d745
Alemhar Salihuddin <alemhar@gmail.com>
2020-03-28 17:21:01 +0800

    Added drop down branch selector on entry form

--- resources/js/components/CashDisbursement.vue
+++ resources/js/components/CashDisbursement.vue
@@ -751,19 +751,9 @@
 
             //setInterval(() => this.loadUsers(),3000);
 
-            $('.modal').on("hidden.bs.modal", function (e) {
-                if($('.modal:visible').length)
-                {
-                    $('.modal-backdrop').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) - 10);
-                    $('body').addClass('modal-open');
-                }
-            }).on("show.bs.modal", function (e) {
-                if($('.modal:visible').length)
-                {
-                    $('.modal-backdrop.in').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) + 10);
-                    $(this).css('z-index', parseInt($('.modal-backdrop.in').first().css('z-index')) + 10);
-                }
-            });
+            $('.modal').on('hidden.bs.modal', function (e) {
+                $('body').addClass('modal-open');
+             });
         },
         components: {
           //DynamicSelect
