26b0c5f7bbd6809abe05fae8be4866823260ae1b
Alemhar Salihuddin <alemhar@gmail.com>
2020-04-04 14:20:57 +0800

    New Item form

--- app/Http/Controllers/API/CDItemController.php
+++ app/Http/Controllers/API/CDItemController.php
@@ -83,12 +83,12 @@ class CDItemController extends Controller
 
     public function list(){
         if ($entry_id = \Request::get('entry_id')) {
-            $accounts = Payee::where(function($query) use ($entry_id){
+            $accounts = TransactionItem::where(function($query) use ($entry_id){
                 $query->where('transaction_entry_id',$entry_id);
             })->paginate(10);
 
         }else{
-            $accounts = Payee::latest()->paginate(10);
+            $accounts = TransactionItem::latest()->paginate(10);
         }
         return $accounts;
     }
