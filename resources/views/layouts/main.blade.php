f11cc16e1eede42d2d7c3942d56808f30f5bb352
Alemhar Salihuddin <alemhar@gmail.com>
2020-04-27 21:20:02 +0800

    Added Readability Option in account settings

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -185,7 +185,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <div class="content">
-      <div class="container-fluid">
+      <div class="container-fluid" style="[readabilityObject]">
         <!-- Main content -->
         <router-view></router-view>
         <vue-progress-bar></vue-progress-bar>
