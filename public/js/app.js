26de70b4cfcfb56c8497f584bbca6d6d5fa08e7f
Alemhar Salihuddin <alemhar@gmail.com>
2020-08-03 21:57:24 +0800

    Added WTax on main Sales form

--- public/js/app.js
+++ public/js/app.js
@@ -9443,7 +9443,7 @@ __webpack_require__.r(__webpack_exports__);
       });
 
       if (this.wTaxExist) {
-        this.form.wtax = (this.form.amount_ex_tax / (this.wTaxExist.tax_rate / 100)).toFixed(2) * 1;
+        this.form.wtax = (this.form.amount_ex_tax * (this.wTaxExist.tax_rate / 100)).toFixed(2) * 1;
       } else {}
     }
   },
