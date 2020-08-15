d8cf9abd2f24094a1736ad903603d69254ee114e
Alemhar Salihuddin <alemhar@gmail.com>
2020-08-15 12:23:21 +0800

    Added Payment modal on Sales form

--- resources/js/components/Payments.vue
+++ resources/js/components/Payments.vue
@@ -135,7 +135,7 @@
       -->
 
 
-          <div  v-show="cd_created" class="box box-warning mt-2">
+          <div  v-show="!cd_created" class="box box-warning mt-2">
             <div class="col-md-12">
               <div class="box">
                 <div class="box-header">
