4a16e114c3d37d6fe461a8271dda12e369b619b1
Alemhar Salihuddin <alemhar@gmail.com>
2020-08-31 21:54:37 +0800

    Create auto depreciation entry

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/test', function () {
             $depreciation = $remainingBalance;
         }
         */
-        array_push($depreciations,$remainingBalance);
+        array_push($depreciations,round($remainingBalance,2));
     }
     return $depreciations;
 });
