53357d72ef6af094dfe892a1ada42507ac86d900
Alemhar Salihuddin <alemhar@gmail.com>
2020-08-31 21:59:32 +0800

    Create auto depreciation entry

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/test', function () {
         
         $depreciation = round($amount/$depreciatiable->useful_life,2);
 
-        $remainingBalance = $amount - 200 - 200;
+        $remainingBalance = $depreciatiable->amount - 200 - 200;
         /*
         if($depreciation > $remainingBalance){
             $depreciation = $remainingBalance;
