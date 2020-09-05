4d84393b423644648f90eb4784327bdccbb789ec
Alemhar Salihuddin <alemhar@gmail.com>
2020-09-06 00:06:11 +0800

    Create General Journal Form

--- public/js/app.js
+++ public/js/app.js
@@ -7808,7 +7808,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       }
     },
     computeInputTax: function computeInputTax(tax_excluded, entity_type, payee_id, branch_id) {
-      this.amount = (tax_excluded / 0.88).toFixed(2) * 1;
+      this.amount = (tax_excluded / 1.88).toFixed(2) * 1;
       this.vat = (this.amount * 0.12).toFixed(2) * 1;
       ++this.transaction_entry_id;
       this.transactions.push({
