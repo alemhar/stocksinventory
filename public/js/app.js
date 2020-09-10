a45d52ab853a4a930e0487f0893063ec4bad775f
Alemhar Salihuddin <alemhar@gmail.com>
2020-09-10 22:23:49 +0800

    Create Transactions Form

--- public/js/app.js
+++ public/js/app.js
@@ -13532,6 +13532,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -91679,103 +91681,93 @@ var render = function() {
   return _c("div", { staticClass: "container" }, [
     _vm.$gate.isAdminOrUser()
       ? _c("div", { staticClass: "row mt-1" }, [
+          _c("div", {}, [
+            _c("div", { staticClass: "input-group" }, [
+              _c("div", { staticClass: "input-group mb-2" }, [
+                _vm._m(0),
+                _vm._v(" "),
+                _c("input", {
+                  directives: [
+                    {
+                      name: "model",
+                      rawName: "v-model",
+                      value: _vm.account_code,
+                      expression: "account_code"
+                    }
+                  ],
+                  staticClass: "form-control col-2",
+                  attrs: {
+                    type: "text",
+                    id: "inputAccountCode",
+                    placeholder: "Code"
+                  },
+                  domProps: { value: _vm.account_code },
+                  on: {
+                    keyup: function($event) {
+                      if (
+                        !$event.type.indexOf("key") &&
+                        _vm._k($event.keyCode, "enter", 13, $event.key, "Enter")
+                      ) {
+                        return null
+                      }
+                      return _vm.loadLedger(_vm.account_code)
+                    },
+                    input: function($event) {
+                      if ($event.target.composing) {
+                        return
+                      }
+                      _vm.account_code = $event.target.value
+                    }
+                  }
+                }),
+                _vm._v(" "),
+                _c("input", {
+                  directives: [
+                    {
+                      name: "model",
+                      rawName: "v-model",
+                      value: _vm.account_name,
+                      expression: "account_name"
+                    }
+                  ],
+                  staticClass: "form-control col-9",
+                  attrs: {
+                    readonly: "true",
+                    type: "text",
+                    id: "inputAccountName",
+                    placeholder: "Account Name"
+                  },
+                  domProps: { value: _vm.account_name },
+                  on: {
+                    input: function($event) {
+                      if ($event.target.composing) {
+                        return
+                      }
+                      _vm.account_name = $event.target.value
+                    }
+                  }
+                }),
+                _vm._v(" "),
+                _c("span", { staticClass: "input-group-btn col-1" }, [
+                  _c(
+                    "button",
+                    {
+                      staticClass: "btn btn-success",
+                      attrs: { type: "button" },
+                      on: { click: _vm.searchAccountModal }
+                    },
+                    [_c("i", { staticClass: "fas fa-search fa-fw" })]
+                  )
+                ])
+              ])
+            ]),
+            _vm._v(" "),
+            _vm._m(1)
+          ]),
+          _vm._v(" "),
           _c("div", { staticClass: "col-md-12" }, [
             _c("div", { staticClass: "box mt-4" }, [
-              _c("div", { staticClass: "box-header" }, [
-                _c("h3", { staticClass: "box-title" }, [
-                  _vm._v("Transactions")
-                ]),
-                _vm._v(" "),
-                _c("div", { staticClass: "box-tools" }, [
-                  _c("div", { staticClass: "input-group" }, [
-                    _c("div", { staticClass: "input-group mb-2" }, [
-                      _vm._m(0),
-                      _vm._v(" "),
-                      _c("input", {
-                        directives: [
-                          {
-                            name: "model",
-                            rawName: "v-model",
-                            value: _vm.account_code,
-                            expression: "account_code"
-                          }
-                        ],
-                        staticClass: "form-control col-2",
-                        attrs: {
-                          type: "text",
-                          id: "inputAccountCode",
-                          placeholder: "Code"
-                        },
-                        domProps: { value: _vm.account_code },
-                        on: {
-                          keyup: function($event) {
-                            if (
-                              !$event.type.indexOf("key") &&
-                              _vm._k(
-                                $event.keyCode,
-                                "enter",
-                                13,
-                                $event.key,
-                                "Enter"
-                              )
-                            ) {
-                              return null
-                            }
-                            return _vm.loadLedger(_vm.account_code)
-                          },
-                          input: function($event) {
-                            if ($event.target.composing) {
-                              return
-                            }
-                            _vm.account_code = $event.target.value
-                          }
-                        }
-                      }),
-                      _vm._v(" "),
-                      _c("input", {
-                        directives: [
-                          {
-                            name: "model",
-                            rawName: "v-model",
-                            value: _vm.account_name,
-                            expression: "account_name"
-                          }
-                        ],
-                        staticClass: "form-control col-9",
-                        attrs: {
-                          readonly: "true",
-                          type: "text",
-                          id: "inputAccountName",
-                          placeholder: "Account Name"
-                        },
-                        domProps: { value: _vm.account_name },
-                        on: {
-                          input: function($event) {
-                            if ($event.target.composing) {
-                              return
-                            }
-                            _vm.account_name = $event.target.value
-                          }
-                        }
-                      }),
-                      _vm._v(" "),
-                      _c("span", { staticClass: "input-group-btn col-1" }, [
-                        _c(
-                          "button",
-                          {
-                            staticClass: "btn btn-success",
-                            attrs: { type: "button" },
-                            on: { click: _vm.searchAccountModal }
-                          },
-                          [_c("i", { staticClass: "fas fa-search fa-fw" })]
-                        )
-                      ])
-                    ])
-                  ]),
-                  _vm._v(" "),
-                  _vm._m(1)
-                ])
-              ]),
+              _vm._m(2),
               _vm._v(" "),
               _c(
                 "div",
@@ -91804,7 +91796,7 @@ var render = function() {
                             _c(
                               "tbody",
                               [
-                                _vm._m(2),
+                                _vm._m(3),
                                 _vm._v(" "),
                                 _vm._l(_vm.ledgers.data, function(
                                   ledger,
@@ -91877,7 +91869,7 @@ var render = function() {
                   },
                   [
                     _c("div", { staticClass: "modal-content" }, [
-                      _vm._m(3),
+                      _vm._m(4),
                       _vm._v(" "),
                       _c("form", { attrs: { onsubmit: "return false;" } }, [
                         _c("div", { staticClass: "modal-body" }, [
@@ -91922,7 +91914,7 @@ var render = function() {
                                   _c(
                                     "tbody",
                                     [
-                                      _vm._m(4),
+                                      _vm._m(5),
                                       _vm._v(" "),
                                       _vm._l(
                                         _vm.chart_of_accounts.data,
@@ -91984,7 +91976,7 @@ var render = function() {
                           )
                         ]),
                         _vm._v(" "),
-                        _vm._m(5)
+                        _vm._m(6)
                       ])
                     ])
                   ]
@@ -92026,6 +92018,14 @@ var staticRenderFns = [
         staticClass: "form-control col-12",
         attrs: { type: "date", id: "inputDate", placeholder: "Date" }
       })
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "box-header" }, [
+      _c("h3", { staticClass: "box-title" }, [_vm._v("Transactions")])
     ])
   },
   function() {
