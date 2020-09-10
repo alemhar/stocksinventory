2e7b5ae8cc85a0ca1941f8a761338c855d457cd7
Alemhar Salihuddin <alemhar@gmail.com>
2020-09-10 22:26:21 +0800

    Create Transactions Form

--- resources/js/components/Transactions.vue
+++ resources/js/components/Transactions.vue
@@ -5,24 +5,26 @@
         
 
         <div class="col-md-12">
-            <div class="">
-                <div class="input-group">
-                <div class="input-group mb-2">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text inputGroup-sizing-default">Account</span>
+            <div class="row">
+                <div class="col-4">
+                    <div class="input-group">
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text inputGroup-sizing-default">Account</span>
+                        </div>
+                        <input type="text" class="form-control col-2" id="inputAccountCode" placeholder="Code" @keyup.enter="loadLedger(account_code)" v-model="account_code">
+                        <input readonly="true" type="text" class="form-control col-9" id="inputAccountName" placeholder="Account Name" v-model="account_name">
+                        <span class="input-group-btn col-1">
+                            <button type="button" class="btn btn-success" @click="searchAccountModal"><i class="fas fa-search fa-fw"></i></button>
+                        </span>
+                        </div>
                     </div>
-                    <input type="text" class="form-control col-2" id="inputAccountCode" placeholder="Code" @keyup.enter="loadLedger(account_code)" v-model="account_code">
-                    <input readonly="true" type="text" class="form-control col-9" id="inputAccountName" placeholder="Account Name" v-model="account_name">
-                    <span class="input-group-btn col-1">
-                        <button type="button" class="btn btn-success" @click="searchAccountModal"><i class="fas fa-search fa-fw"></i></button>
-                    </span>
+                    <div class="input-group mb-2">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text inputGroup-sizing-default">Date</span>
+                        </div>
+                        <input  type="date"  class="form-control col-12" id="inputDate" placeholder="Date">
                     </div>
-                </div>
-                <div class="input-group mb-2">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text inputGroup-sizing-default">Date</span>
-                    </div>
-                    <input  type="date"  class="form-control col-12" id="inputDate" placeholder="Date">
                 </div>
             </div>
             
