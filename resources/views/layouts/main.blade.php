676733077ddf276e3e46d844352f93d2c614fc84
Alemhar Salihuddin <alemhar@gmail.com>
2020-09-15 21:29:30 +0800

    Added Companies Table

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -289,10 +289,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      aiabooks.com
+      aiacloudapps.com
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2020 <a href="https://aiacloudapps.com">AIA Cloud Apps</a>.</strong> All rights reserved.
   </footer>
 </div>
 
