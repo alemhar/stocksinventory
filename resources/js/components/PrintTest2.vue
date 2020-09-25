ec6f4bdd1578fd0b474854bffdf3f4d781cc079f
Alemhar Salihuddin <alemhar@gmail.com>
2020-09-26 00:18:35 +0800

    Balance Sheet

--- resources/js/components/PrintTest2.vue
+++ resources/js/components/PrintTest2.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div class="container">
-        <loading :active.sync="isLoading" :is-full-page="fullPage" :height="128" :width="128"></loading>
+        <loading :active.sync="isLoading" :is-full-page="fullPage" :height="128" :width="128" :color="#3490dc"></loading>
         <div class="row mt-1" v-if="$gate.isAdminOrUser()">
             <div class="col-md-12">
                 <!-- div class="row mt-3">
