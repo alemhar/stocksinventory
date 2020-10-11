960b2dbc9013c277893b7368e9142e31b3359e48
Alemhar Salihuddin <alemhar@gmail.com>
2020-10-12 07:52:14 +0800

    Create API Tokens

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -21,7 +21,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::middleware('auth:api')->get('/efps', function (Request $request) {
     return $request;
 });
-
+*/
 
 Route::apiResources([
     'user' => 'API\UserController'
