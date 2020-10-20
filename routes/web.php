21313f73bdc5f4898a0ff4b22b6c6fe265db49de
Alemhar Salihuddin <alemhar@gmail.com>
2020-10-20 23:00:50 +0800

    Depreciation

--- routes/web.php
+++ routes/web.php
@@ -160,9 +160,8 @@ Route::get('/test2', function () {
     
 
     foreach($depreciatiables as $depreciatiable){
+        dd(abs(strtotime($previous_month_last_date) - strtotime($depreciatiable->transaction_date)));
         
-        dd(date($previous_month_last_date) - date($depreciatiable->transaction_date));
-
         if($depreciatiable->transaction_date){
             continue;
         }
